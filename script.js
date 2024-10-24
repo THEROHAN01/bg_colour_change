893f5e806cd1c4ab85c84edf27bd842b98c19a44
Rohan <rohansalunkhe700@gmail.com>
2024-10-24 16:04:12 +0530

    "created the basic on click functionality bg change app "

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+// dom manipulation
+function changeToVoilet(){
+    document.querySelector("body").style.backgroundColor = "violet";
+}
+function changeToIndigo(){
+    document.querySelector("body").style.backgroundColor = "rgb(169, 88, 227)";
+}
+function changeToBlue(){
+    document.querySelector("body").style.backgroundColor = "rgb(87, 87, 234)";
+}
+function changeToGreen(){
+    document.querySelector("body").style.backgroundColor = "rgb(88, 227, 88)";
+}
+function changeToYellow(){
+    document.querySelector("body").style.backgroundColor = "#ffe54c";
+}
+function changeToOrange(){
+    document.querySelector("body").style.backgroundColor = "rgb(252, 180, 45)";
+}
+function changeToRed(){
+    document.querySelector("body").style.backgroundColor = "rgb(248, 66, 66)";
+}
+function changeToWish(){
+    let colour = document.getElementById("#inputWish").value ;
+    
+}
+
+  /* new problem ; add custmom colours to the colour pannel */
